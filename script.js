5b0f22f36e31ed5aa9def65bb4549b64a3bcd668
Jad <57455890+Jad-Adams@users.noreply.github.com>
2024-04-08 18:43:49 +0200

    Improved UX
    Key enter does not trigger unless question input has a value.
    
    Questions converted to lower case
    
    Machine plays question back to user, and announces it is "thinking about it"
    
    Added Easter egg - Oeb replays Oeb
    
    Added delay to text appearing

--- script.js
+++ script.js
@@ -2,29 +2,41 @@ const questionInput = document.getElementById(`question-input`);
 const askBtn = document.querySelector(`.ask-btn`);
 const answerText = document.querySelector(`.answer`);
 const userQuestion = document.querySelector(`.user-question`);
+const thinkingText = document.querySelector(`.thinking`);
 
 askBtn.addEventListener(`click`, questionAnswer);
 
 document.addEventListener(`keydown`, function (keyPress) {
-  if (keyPress.key === `Enter`) {
+  if (keyPress.key === `Enter` && questionInput.value) {
     questionAnswer();
   }
 });
 
 function questionAnswer() {
   let question = questionInput.value;
+  question.toLowerCase();
+
   let secretNumber = Math.trunc(Math.random() * 2 + 1);
-  console.log(secretNumber); //so i can see the answer for testing
 
-  userQuestion.textContent = `Your question was "${question}"`;
+  userQuestion.textContent = `Your question was "${question}?"`;
 
-  if (secretNumber === 1) {
-    answerText.textContent = `No`;
-    console.log(`No`);
-  } else {
-    console.log(`Yes`);
-    answerText.textContent = `Yes`;
+  function machineThinking() {
+    thinkingText.textContent = `The machine is thinking about it`;
   }
+
+  function displayAnswer() {
+    if (secretNumber === 1) {
+      answerText.textContent = `No`;
+    } else if (secretNumber === 2) {
+      answerText.textContent = `Yes`;
+    }
+    if (question === `oeb` || `oeb?`) {
+      answerText.textContent = `Oeb?`;
+    }
+  }
+
+  setTimeout(machineThinking, 1000);
+  setTimeout(displayAnswer, 3000);
 
   console.log(`User question: ${question}`);
   return question;
